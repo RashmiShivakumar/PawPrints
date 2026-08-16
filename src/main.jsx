27d98ecd135eac972f4aa82f3e78aa8aa722b8b8
RashmiShivakumar <81591271+RashmiShivakumar@users.noreply.github.com>
2026-08-16 14:56:50 -0700

    Add React application entry point

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import PawBookApp from "./App.jsx";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <PawBookApp />
+  </React.StrictMode>
+);
